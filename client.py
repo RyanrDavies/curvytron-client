7a0ae138c778a71d6eff6d6ddd05d3a6a0c642a8
Ryan Davies <ryanrenniedavies@gmail.com>
2018-03-15 01:01:18 +0000

    feat: Finish function to join game, add helper functions for receiving messages
    Client can now join a room, or create it if the room doesn't exist.
    
    Added functions to wait for a reply. Might want to change main run function to
    make use of some of these.
    
    May want to reconsider the way messages are treated -- received messages are
    more like a list of messages.

--- client.py
+++ client.py
@@ -2,16 +2,22 @@ import websocket
 import json
 import threading
 
+websocket.enableTrace(False)
+
 
 class CurvytronClient(threading.Thread):
 
-    FETCH_ROOMS = '[[room:fetch]]'
+    FETCH_ROOMS = '[["room:fetch"]]'
     WHOAMI = '[["whoami",null,0]]'
-    MAKE_ROOM = '[["room:create",{{"name":"{name}"}},{msg_id}]]'
+    MAKE_ROOM = '[["room:create",{{"name":"{room_name}"}},{msg_id}]]'
     PLAYER_MOVE = '[["player:move",{{"avatar":{player_id},"move":{action}}}]]'
+    JOIN_ROOM = '[["room:join",{{"name":"{room_name}","password":null}},{msg_id}]]'
+    ADD_PLAYER = '[["player:add",{{"name":"{player_name}","color":"{player_color}"}},{msg_id}]]'
 
-    def __init__(self, server, name='pythonClient', color="#ffffff", room=None):
+    def __init__(self, server, name='pythonClient', color="#ffffff", room=''):
         super(CurvytronClient, self).__init__()
+
+        self.message_id = -1
 
         self.ws = websocket.WebSocket()
         self.alive = threading.Event()
@@ -26,10 +32,9 @@ class CurvytronClient(threading.Thread):
         self.player_id = None
         self._connect_to_server(server=server)
         self._join_room(room)
-        self.message_id = 0
-        self.ws.settimeout(0.05)
 
     def run(self):
+        self.ws.settimeout(0.05)
         while self.alive.isSet():
             try:
                 message = self.ws.recv()
@@ -57,6 +62,40 @@ class CurvytronClient(threading.Thread):
         response = self.ws.recv()
         self.client_id = int(response.split(',')[-1][:-2])
 
+    def _recv_message(self,timeout=None,default=None):
+        if timeout:
+            prev_timeout = self.ws.gettimeout()
+            self.ws.settimeout(timeout)
+            try:
+                message = json.loads(self.ws.recv())
+            except websocket.WebSocketTimeoutException:
+                message=default
+            finally:
+                self.ws.settimeout(prev_timeout)
+        else:
+            message = json.loads(self.ws.recv())
+        return message
+
+    def _wait_for_message(self,message_type=None,**kwargs):
+        message =  self._recv_message(**kwargs)
+        if kwargs.has_key('default') and message == kwargs['default']:
+            return message
+        if message_type == message[0][0]:
+            return message
+        else:
+            self._parse_message(message)
+            return self._wait_for_message(message_type,**kwargs)
+
+    def _wait_for_reply(self,**kwargs):
+        message =  self._recv_message(**kwargs)
+        if kwargs.has_key('default') and message == kwargs['default']:
+            return message
+        if message[0][0] == self.message_id:
+            return message
+        else:
+            self._parse_message(message)
+            return self._wait_for_reply(**kwargs)
+
     def _join_room(self, target_room):
         """Doc string
         If the provided room exists, join it.
@@ -67,18 +106,28 @@ class CurvytronClient(threading.Thread):
         """
 
         self._send_message(self.FETCH_ROOMS, {})
-        rooms = self.ws.recv()
-        rooms = json.loads(rooms)
+        rooms = self._wait_for_message("room:open",timeout=1,default=[])
         for room in rooms:
             if room[1]['name'] == target_room:
                 break
         else:
             # create room
-            pass
+            self._send_message(self.MAKE_ROOM, {'room_name': target_room})
+            response = self._wait_for_reply()
+            assert(response[0][1]['success'])
 
         # join room
-        self.player_id = None
-        pass
+        self._send_message(self.JOIN_ROOM, {'room_name': target_room})
+        response = self._wait_for_reply()
+        assert(response[0][1]['success'])
+        self._send_message(self.ADD_PLAYER, {})
+        response = self._wait_for_reply()
+        print "recevied message", response
+        assert (response[0][1]['success'])
+        response = self._wait_for_message('room:join')
+        if (response[0][0] == 'room:join' and response[0][1]['player']['client'] == self.client_id
+                and response[0][1]['player']['name'] == self.name):
+            self.player_id = response[0][1]['player']['id']
 
     def _send_message(self, message, message_args):
         """
@@ -88,8 +137,8 @@ class CurvytronClient(threading.Thread):
         :param message_args:
         :return:
         """
+        self.message_id += 1
         self.ws.send(message.format(msg_id=self.message_id, client_id=self.client_id, player_id=self.player_id,
-                                    name=self.name, **message_args))
-
+                                    player_name=self.name, player_color=self.color, **message_args))
     def send_action(self, action):
         self._send_message(self.PLAYER_MOVE, {'action': action})
